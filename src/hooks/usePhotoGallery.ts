bc889947b2eebd94fcc732b26b275e9f86310dd4
Alex Q. <alexsam.quentin@gmail.com>
2023-11-07 21:58:41 +0100

    Saving Photos

--- src/hooks/usePhotoGallery.ts
+++ src/hooks/usePhotoGallery.ts
@@ -6,7 +6,24 @@ import { Filesystem, Directory } from '@capacitor/filesystem';
 import { Preferences } from '@capacitor/preferences';
 import { Capacitor } from '@capacitor/core';
 
+
 export function usePhotoGallery() {
+  const savePicture = async (photo: Photo, fileName: string): Promise<UserPhoto> => {
+    const base64Data = await base64FromPath(photo.webPath!);
+    const savedFile = await Filesystem.writeFile({
+      path: fileName,
+      data: base64Data,
+      directory: Directory.Data,
+    });
+
+    // Use webPath to display the new image instead of base64 since it's
+    // already loaded into memory
+    return {
+      filepath: fileName,
+      webviewPath: photo.webPath,
+    };
+  };
+
   const [photos, setPhotos] = useState<UserPhoto[]>([]);
   const takePhoto = async () => {
     const photo = await Camera.getPhoto({
@@ -14,25 +31,39 @@ export function usePhotoGallery() {
       source: CameraSource.Camera,
       quality: 100,
     });
-    
+  
     const fileName = Date.now() + '.jpeg';
-    const newPhotos = [
-      {
-        filepath: fileName,
-        webviewPath: photo.webPath,
-      },
-      ...photos,
-    ];
+    const savedFileImage = await savePicture(photo, fileName);
+    const newPhotos = [savedFileImage, ...photos];
     setPhotos(newPhotos);
   };
-
-
 
   return {
     photos,
     takePhoto,
   };
+
 }
+
+export async function base64FromPath(path: string): Promise<string> {
+  const response = await fetch(path);
+  const blob = await response.blob();
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.onerror = reject;
+    reader.onload = () => {
+      if (typeof reader.result === 'string') {
+        resolve(reader.result);
+      } else {
+        reject('method did not return a string');
+      }
+    };
+    reader.readAsDataURL(blob);
+  });
+
+  
+}
+
 
 export interface UserPhoto {
   filepath: string;
