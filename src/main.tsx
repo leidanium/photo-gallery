ea1082c37b44d0208b274bd2c988be12cec12833
Alex Q. <alexsam.quentin@gmail.com>
2023-11-07 08:44:37 +0100

    Step One - Renaming

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
+import { defineCustomElements } from '@ionic/pwa-elements/loader';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 
+defineCustomElements(window);
 const container = document.getElementById('root');
 const root = createRoot(container!);
 root.render(
--- src/pages/Tab2.tsx
+++ src/pages/Tab2.tsx
@@ -1,4 +1,19 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { camera, trash, close } from 'ionicons/icons';
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  IonFab,
+  IonFabButton,
+  IonIcon,
+  IonGrid,
+  IonRow,
+  IonCol,
+  IonImg,
+  IonActionSheet,
+} from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab2.css';
 
@@ -13,9 +28,16 @@ const Tab2: React.FC = () => {
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Tab 2</IonTitle>
+            <IonTitle size="large">Photo Both</IonTitle>
           </IonToolbar>
         </IonHeader>
+        <IonContent>
+        <IonFab vertical="bottom" horizontal="center" slot="fixed">
+          <IonFabButton onClick={() => takePhoto()}>
+            <IonIcon icon={camera}></IonIcon>
+          </IonFabButton>
+        </IonFab>
+      </IonContent> 
         <ExploreContainer name="Tab 2 page" />
       </IonContent>
     </IonPage>
